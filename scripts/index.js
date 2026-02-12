fb224cd295512c2a4a12c3ffc1a8e96d6d0a6b64
Erick Jimenez <esjimenezs@hotmail.com>
2026-02-11 19:38:49 -0500

    feat: agregando funcionalidad al boton editar perfil

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,47 @@
+const editProfile = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const popupClose = editPopup.querySelector(".popup__close");
+const inputName = editPopup.querySelector(".popup__input_type_name");
+const profileTitle = document.querySelector(".profile__title");
+const inputDescription = editPopup.querySelector(
+  ".popup__input_type_description",
+);
+const profileDescription = document.querySelector(".profile__description");
+const saveBtn = editPopup.querySelector(".popup__button");
+const formElement = editPopup.querySelector("#edit-profile-form");
+
+function openModal(editPopup) {
+  editPopup.classList.add("popup_is-opened");
+}
+function closeModal(editPopup) {
+  editPopup.classList.remove("popup_is-opened");
+}
+function fillProfileForm() {
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+function handleOpenEditModal() {
+  openModal(editPopup);
+  fillProfileForm();
+}
+
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+  const nameValue = inputName.value;
+  const jobValue = inputDescription.value;
+
+  profileTitle.textContent = nameValue;
+  profileDescription.textContent = jobValue;
+  closeModal(editPopup);
+}
+
+editProfile.addEventListener("click", handleOpenEditModal);
+popupClose.addEventListener("click", (e) => {
+  closeModal(editPopup);
+});
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
 const initialCards = [
   {
     name: "Valle de Yosemite",
