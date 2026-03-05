9f696cb55947520f4044dffd38da0bf0fdd4f28c
Erick Jimenez <esjimenezs@hotmail.com>
2026-03-04 19:23:46 -0500

    feat: add functionality of validity of popups changes in popup.css, index.html and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -150,14 +150,14 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add("popup__input_type_error");
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__form__input-error_active");
+  errorElement.classList.add("popup__input-error_active");
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove("popup__input_type_error");
   errorElement.textContent = "";
-  errorElement.classList.remove("popup__form__input-error_active");
+  errorElement.classList.remove("popup__input-error_active");
 };
 
 function toggleButtonState(inputList, buttonElement) {
