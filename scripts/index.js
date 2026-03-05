2c1f78ed1bcd576bdbebc74b4f3b78764288546a
Erick Jimenez <esjimenezs@hotmail.com>
2026-03-04 19:48:16 -0500

    add functionality of validity of popups changes in popup.css, index.html and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -147,14 +147,14 @@ initialCards.forEach(function (initialCard) {
 
 //funciones para validación de campos
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.add("popup__input_type_error");
   errorElement.textContent = errorMessage;
   errorElement.classList.add("popup__input-error_active");
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove("popup__input_type_error");
   errorElement.textContent = "";
   errorElement.classList.remove("popup__input-error_active");
